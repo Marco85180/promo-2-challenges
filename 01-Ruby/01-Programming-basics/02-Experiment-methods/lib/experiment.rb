520461a43650cd0ce060889f06336929c3adb7f2
MARCO <lefollmarc@hotmail.fr>
2014-04-08 11:41:10 +0200

    3thd exercice

--- 01-Ruby/01-Programming-basics/02-Experiment-methods/lib/experiment.rb
+++ 01-Ruby/01-Programming-basics/02-Experiment-methods/lib/experiment.rb
@@ -17,6 +17,7 @@ end
 def replace(initial_string, old_letter, new_letter)
   # TODO: return a copy of the string with the new letter replacing the old one
   # example: replace("casanova", "a", "o") => "cosonovo"
+initial_string.tr('old_letter','new_letter')
 end
 
 def exactly_divide(an_integer, a_divider)
