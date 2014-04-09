7eb7d45c5ed33e2f093fa5e6ae9ead18c8c5496a
MARCO <lefollmarc@hotmail.fr>
2014-04-09 21:21:11 +0200

    That's finish

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -10,6 +10,11 @@ def coach_answer(your_message)
 end
 
 def coach_answer_enhanced(your_message)
+  answer = coach_answer(your_message)
   # TODO: return coach answer to your_message, with additional custom rules of yours !
+  if your_message == your_message.upcase
+    "I can feel your motivation son"
+  else
+    answer
+  end
 end
-
--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -2,6 +2,11 @@ require_relative "coach_answer"
 #TODO: Implement the program that makes you discuss with your coach from the terminal.
 
 puts "WTF?"
-your_message = gets.chomp
+begin
+  print "> "
+  your_message = gets.chomp
+  puts coach_answer_enhanced(your_message)
 
-puts coach_answer(your_message)
+end until your_message == "I am going to work right now SIR !"
+
+puts "Great!! Bye"
