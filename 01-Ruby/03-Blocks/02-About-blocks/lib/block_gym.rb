e9490eb25e7ef73698af93e090c5835d61083daf
MARCO <lefollmarc@hotmail.fr>
2014-04-10 12:52:15 +0200

    presque

--- 01-Ruby/03-Blocks/02-About-blocks/lib/block_gym.rb
+++ 01-Ruby/03-Blocks/02-About-blocks/lib/block_gym.rb
@@ -1,10 +1,15 @@
 def tag(tag_name, attr = nil)
   #TODO:  Build HTML tags around  content given in the block
   #       The method can be called with an optional HTML attribute inputted as [attr_name, attr_value]
+  tag(tag_name){attr}
 end
 
 def timer_for
   #TODO:  Return time taken to execute the given block
+  beginning_time = Time.now
+  yield
+  end_time = Time.now
+  duration = end_time - beginning_time
 end
 
 def transform(element)
