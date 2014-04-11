702f8e7cd85285791f0920e7aee768c2d2b01085
MARCO <lefollmarc@hotmail.fr>
2014-04-11 06:39:13 +0200

    begin

--- 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
+++ 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
@@ -1,5 +1,25 @@
 # Encoding: utf-8
 
-def louchebemize(sentence)
-  #TODO: implement your louchebem translator
+def insert_l(word)
+vowels = %w(a e y u i o)
+  if vowels.include? word[0]
+  "l" + word
+  end
 end
+
+def suffixes(word)
+suffixes = %w(em, é, ji, oc, ic, uche, ès)
+suffixes[rand(0..suffixes.lenght-1)]
+end
+
+def louchebemize(word)
+  #TODO: implement your louchebem translator
+  if word.length == 1
+    louchebemize_word = word
+  else
+    if
+
+end
+
+puts louchebemize_word(salut)
+
