1be5bf576d1106dde2e487c8a79d1ceae45d8499
MARCO <lefollmarc@hotmail.fr>
2014-04-17 17:49:46 +0200

    First attempt, solved it

--- 02-OOP/05-Dessert-inheritance/lib/dessert.rb
+++ 02-OOP/05-Dessert-inheritance/lib/dessert.rb
@@ -1,15 +1,34 @@
 class Dessert
+
+  attr_reader :calories, :name, :flavor
+
+
   def initialize(name, calories)
-  end 
-  
-  def healthy?
+    @name = name
+    @calories = calories
   end
-  
+
+  def healthy?
+    @calories < 200 ? true : false
+  end
+
   def delicious?
+    true
   end
 end
 
-class JellyBean < Dessert  
+class JellyBean < Dessert
+  attr_reader :flavor
+
   def initialize(name, calories, flavor)
+    super(name, calories)
+    @flavor = flavor
+  end
+
+  def delicious?
+    @flavor == "black licorice" ? false : true
   end
 end
+
+#my_dessert = Dessert.new()
+#p "my_dessert"
