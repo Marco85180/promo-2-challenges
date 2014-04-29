139052e71709e8803c2ba7476a137083e154fd04
MARCO <lefollmarc@hotmail.fr>
2014-04-29 09:32:19 +0200

    First attempt, solved it

--- 03-AR-Database/08-Hackernews-associations/lib/interface.rb
+++ 03-AR-Database/08-Hackernews-associations/lib/interface.rb
@@ -14,30 +14,48 @@ while true
 
   until logged_in
     puts "Please login with your <id>"
-    #TODO: instantiate a user with his <id>
+    user_id = gets.chomp.to_i
+
+    logged_in = true
+
+    begin
+      user = User.find(user_id)
+    rescue
+      puts "Incorrect ID!!"
+      logged_in = false
+    end
   end
 
-  puts "Hey #{user.name}, what do you want to do today? Enter <task_id>"
-  puts "1. Create a post"
-  puts "2. Read your posts"
-  puts "3. Delete all posts"
-  puts "4. Exit"
-  
-	choice =  gets.chomp.to_i
-	
-	case choice
-  when 1
-    name = ask_and_get("name")
-    source_url = ask_and_get("source url")
-    rating = ask_and_get("rating")
-    post = { name: name, source_url: source_url, date: Time.now, rating: rating }
-    #TODO: use ActiveRecord to add a new post for the user logged in!
-  when 2
-    #TODO: use ActiveRecord to get all posts of the current user
-  when 3
-    #TODO: use ActiveRecord to delete all posts of current user 
-  when 4 
-    break
-	end 
-	
+  while true
+      puts "Hey #{user.name}, what do you want to do today? Enter <task_id>"
+      puts "1. Create a post"
+      puts "2. Read your posts"
+      puts "3. Delete all posts"
+      puts "4. Exit"
+
+    	choice =  gets.chomp.to_i
+
+    	case choice
+      when 1
+        name = ask_and_get("name")
+        source_url = ask_and_get("source url")
+        rating = ask_and_get("rating")
+        post = { name: name, source_url: source_url, date: Time.now,
+          rating: rating }
+
+        user.posts.create(post)
+        #TODO: use ActiveRecord to add a new post for the user logged in!
+      when 2
+        user.posts.each do |p|
+          puts p
+        end
+        #TODO: use ActiveRecord to get all posts of the current user
+      when 3
+        user.posts.destroy_all
+        #TODO: use ActiveRecord to delete all posts of current user
+      when 4
+        break
+    	end
+  end
+
 end
--- 03-AR-Database/08-Hackernews-associations/lib/models/post.rb
+++ 03-AR-Database/08-Hackernews-associations/lib/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ActiveRecord::Base
   #TODO: add association to the user model
+  belongs_to :user
+
+  def to_s
+      "#{name} on #{date.strftime('%d/%M/%Y')}, rated #{rating}}"
+  end
+
 end
