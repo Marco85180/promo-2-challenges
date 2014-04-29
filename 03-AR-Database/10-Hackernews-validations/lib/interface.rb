8a6567bbf11d20f3d3d6f6cb95c22d9739eb6a1c
MARCO <lefollmarc@hotmail.fr>
2014-04-29 16:16:14 +0200

    First attempt, solved it

--- 03-AR-Database/10-Hackernews-validations/lib/interface.rb
+++ 03-AR-Database/10-Hackernews-validations/lib/interface.rb
@@ -1,3 +1,61 @@
 require_relative 'config/application'
 require './models/post'
 require './models/user'
+
+#TODO: build your interface with login and CRUD tasks on the posts
+def ask_and_get(param)
+  puts "What is the #{param} of your piece of news?"
+  gets.chomp
+end
+
+while true
+
+  logged_in = false
+
+  until logged_in
+    puts "Please login with your <id>"
+    user_id = gets.chomp.to_i
+
+    logged_in = true
+
+    begin
+      user = User.find(user_id)
+    rescue
+      puts "Incorrect ID!!"
+      logged_in = false
+    end
+  end
+
+  while true
+      puts "Hey #{user.name}, what do you want to do today? Enter <task_id>"
+      puts "1. Create a post"
+      puts "2. Read your posts"
+      puts "3. Delete all posts"
+      puts "4. Exit"
+
+      choice =  gets.chomp.to_i
+
+      case choice
+      when 1
+        name = ask_and_get("name")
+        source_url = ask_and_get("source url")
+        rating = ask_and_get("rating")
+        post = { name: name, source_url: source_url, date: Time.now,
+          rating: rating }
+
+        user.posts.create(post)
+        #TODO: use ActiveRecord to add a new post for the user logged in!
+      when 2
+        user.posts.each do |p|
+          puts p
+        end
+        #TODO: use ActiveRecord to get all posts of the current user
+      when 3
+        user.posts.destroy_all
+        #TODO: use ActiveRecord to delete all posts of current user
+      when 4
+        break
+      end
+  end
+
+end
