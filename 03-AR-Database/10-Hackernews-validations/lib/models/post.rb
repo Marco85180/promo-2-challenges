ef1199b021e92b82b56233d3fe3856c08fb2287f
MARCO <lefollmarc@hotmail.fr>
2014-04-29 16:09:34 +0200

    First attempt, solved it

--- 03-AR-Database/10-Hackernews-validations/lib/models/post.rb
+++ 03-AR-Database/10-Hackernews-validations/lib/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+  validates :name, :date, :source_url, :rating, :user, presence: true
+  validates :name, uniqueness: true
+  validates :rating, numericality: { greater_than_or_equal_to: 0 }
 end
--- 03-AR-Database/10-Hackernews-validations/lib/models/user.rb
+++ 03-AR-Database/10-Hackernews-validations/lib/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   has_many :posts
+  validates :name, presence: true
+  validates :email, format: { with /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
+message: "Please enter a email"}
 end
