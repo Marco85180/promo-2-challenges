a58dc0b4ce44501a5bda8e27f725eeedba679e47
MARCO <lefollmarc@hotmail.fr>
2014-05-05 16:33:19 +0200

    finish

--- 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
+++ 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
@@ -3,3 +3,22 @@
   * TODO: add your code here!
   *
 */
+
+$(document).ready(function() {
+  $("#message").hide();
+  $("#message").slideDown("slow");
+});
+
+
+$("#message-wrapper").click(function( event ) {
+  event.preventDefault();
+  $( "#message" ).slideUp("slow");
+
+  $("#message-wrapper").hover(function(){
+    $( "#message" ).slideDown( 100 );
+  },
+  function(){
+    $( "#message" ).slideUp( 500 );
+  })
+
+});
