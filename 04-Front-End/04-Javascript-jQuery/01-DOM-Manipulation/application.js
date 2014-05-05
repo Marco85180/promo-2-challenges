ee6afef56f8faa6ceec2ed8ada2a7f61948109a4
MARCO <lefollmarc@hotmail.fr>
2014-05-05 15:26:01 +0200

    finish

--- 04-Front-End/04-Javascript-jQuery/01-DOM-Manipulation/application.js
+++ 04-Front-End/04-Javascript-jQuery/01-DOM-Manipulation/application.js
@@ -8,47 +8,57 @@
 function run_challenges() {
 
   // Ex 1. Read the content of the email input
-  var email = null;  // TODO: replace null, keep the email variable.
+  var email = $("#email").val();  // TODO: replace null, keep the email variable.
 
 
 
   // Ex 2. Fill the content of the email input with your email
-
+  $("#email").val('le_foll_marc');
 
 
   // Ex 3. Replace the email hint (next to the input) with 'This is my email now'
   //       The text should be emphasized using a <strong> tag
-
+  $("#email-hint").html("<strong>This is my email now</strong>")
 
   // Ex 4. Add the .blue CSS class to the table header cells (th elements)
-
+  $('tr > th').css({ 
+    color: 'rgb(0, 0, 255)'  }); 
 
 
   // Ex 5. Count the number of table body rows there are (team count!)
-  var team_count = 0;  // TODO: replace 0, keep the team_count variable.
+  var team_count = $('tbody > tr').size();  // TODO: replace 0, keep the team_count variable.
 
 
 
   // Ex 6. Say there is a 15th team added to the table.
   //       Add a row at the bottom, this new team should have zero points.
-
+  var add_team = $('tbody').append("<tr><td>15</td><td>New team</td><td>0</td></tr>");
 
 
   // Ex 7. Write some code to sum all points given to all teams
-  var sum = 0;  // TODO: replace 0 with your sum result, keep the sum variable.
-
+  var sum = 0;
+  $('tbody > tr').each(function(i, el) {
+    num = parseInt($(el).find("td:nth-child(3)").text())
+    sum += num;
+  });
+  // var add_sum = $('tbody').append("<tr><td>16</td><td>Total</td><td>#{sum}</td></tr>")
 
 
   // Ex 8. Change the background color of all table header cells to #DDF4FF
-
+  $('tr > th').css({ 
+    background: '#DDF4FF'  }); 
 
 
   // Ex 9. Translate the #results list 20px downward (animation would be great ^_^)
-
+  $("#results").animate({ width: "100%",
+    opacity: 0.6,
+    top: "20px",
+    fontSize: "2em",
+  }, 500 );
 
 
   // Ex 10. Remove the "Email:" label from the DOM
-
+  $('div > label').remove();
 
 
   // Checking exercise answers. DO NOT MODIFY BELOW
